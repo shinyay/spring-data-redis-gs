c0a4dca4d637631254c3d64a7b923a5857e93ad0
shinyay <shinya.com@gmail.com>
2020-08-10 09:50:52 +0900

    Add Redis Configuration Class

---
+++ src/main/kotlin/com/google/shinyay/configuration/RedisConfiguration.kt
@@ -0,0 +1,32 @@
+package com.google.shinyay.configuration
+
+import com.google.shinyay.messaging.Receiver
+import org.springframework.context.annotation.Bean
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.core.StringRedisTemplate
+import org.springframework.data.redis.listener.PatternTopic
+import org.springframework.data.redis.listener.RedisMessageListenerContainer
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter
+
+class RedisConfiguration {
+
+    @Bean
+    fun receiver() = Receiver()
+
+    @Bean
+    fun redisTemplate(connectionFactory: RedisConnectionFactory)
+            = StringRedisTemplate(connectionFactory)
+
+    @Bean
+    fun listenerAdapter(receiver: Receiver)
+            = MessageListenerAdapter(receiver, "receiveMessage")
+
+    @Bean
+    fun listenerContainer(connectionFactory: RedisConnectionFactory,
+                          listenerAdapter: MessageListenerAdapter): RedisMessageListenerContainer {
+        return RedisMessageListenerContainer().apply {
+            setConnectionFactory(connectionFactory)
+            addMessageListener(listenerAdapter, PatternTopic("chat"))
+        }
+    }
+}
