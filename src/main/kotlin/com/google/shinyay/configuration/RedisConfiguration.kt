eb8ffa0d565380b8796750112d366290fbc246f2
shinyay <shinya.com@gmail.com>
2020-08-10 21:36:59 +0900

    Add Configuration Annotation for component scanner

--- src/main/kotlin/com/google/shinyay/configuration/RedisConfiguration.kt
+++ src/main/kotlin/com/google/shinyay/configuration/RedisConfiguration.kt
@@ -2,12 +2,14 @@ package com.google.shinyay.configuration
 
 import com.google.shinyay.messaging.Receiver
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.core.StringRedisTemplate
 import org.springframework.data.redis.listener.PatternTopic
 import org.springframework.data.redis.listener.RedisMessageListenerContainer
 import org.springframework.data.redis.listener.adapter.MessageListenerAdapter
 
+@Configuration
 class RedisConfiguration {
 
     @Bean
