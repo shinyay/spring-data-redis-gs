cd355e72e2b83839d0b5a5eeabc1dd9c40a82910
shinyay <shinya.com@gmail.com>
2020-08-10 21:07:18 +0900

    Add message send to redis

--- src/main/kotlin/com/google/shinyay/SpringDataRedisGsApplication.kt
+++ src/main/kotlin/com/google/shinyay/SpringDataRedisGsApplication.kt
@@ -1,16 +1,29 @@
 package com.google.shinyay
 
+import com.google.shinyay.messaging.Receiver
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.ApplicationContext
+import org.springframework.data.redis.core.StringRedisTemplate
+import kotlin.system.exitProcess
 
 @SpringBootApplication
 class SpringDataRedisGsApplication
 
 fun main(args: Array<String>) {
-	runApplication<SpringDataRedisGsApplication>(*args)
+	val ctx: ApplicationContext = runApplication<SpringDataRedisGsApplication>(*args)
+	val redisTemplate = ctx.getBean(StringRedisTemplate::class.java)
+	val receiver = ctx.getBean(Receiver::class.java)
+
+	while (receiver.getCount() == 0) {
+		redisTemplate.convertAndSend("chat", "Hello from Redis")
+		Thread.sleep(500L)
+	}
+	exitProcess(0)
 }
+
 
 val Any.logger: Logger
 	get() = LoggerFactory.getLogger(this.javaClass)
